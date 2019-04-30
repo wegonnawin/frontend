0bfb4cecdfd3e9e5d7219b6c26822d1604b3d09a
ENvironmentSet <sbshw1@gmail.com>
2019-04-30 00:52:13 -0700

    Add semicolon to card.js

--- src/components/atoms/card.js
+++ src/components/atoms/card.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import styled from 'styled-components'
-import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
-import Typography from '@material-ui/core/Typography'
+import React from 'react';
+import styled from 'styled-components';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 const StyledGrid = styled(Grid)`
     width: 184px;
     max-width: 184px;
     height: 210px;
-`
+`;
 const StyledPaper = styled(Paper)`
     padding-top: theme.spacing.unit * 10;
     padding-bottom: theme.spacing.unit * 10;
     height: 100%;
     margin: auto;
-`
+`;
 
 const Card = () => (
     <StyledGrid>
@@ -24,6 +24,6 @@ const Card = () => (
         </Typography>
     </StyledPaper>
     </StyledGrid>
-)
+);
 
-export default Card
+export default Card;
