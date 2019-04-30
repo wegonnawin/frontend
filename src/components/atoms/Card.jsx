1c90ac0447d872e8a5bda7359ff2570c04d862a8
ENvironmentSet <sbshw1@gmail.com>
2019-04-30 00:58:49 -0700

    fix Card

--- src/components/atoms/Card.jsx
+++ src/components/atoms/Card.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
+
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 const StyledGrid = styled(Grid)`
-    width: 184px;
-    max-width: 184px;
-    height: 210px;
-`;
-const StyledPaper = styled(Paper)`
-    padding-top: theme.spacing.unit * 10;
-    padding-bottom: theme.spacing.unit * 10;
-    height: 100%;
-    margin: auto;
+  width: 184px;
+  max-width: 184px;
+  height: 210px;
 `;
 
-const Card = () => (
-    <StyledGrid>
+const StyledPaper = styled(Paper)`
+  padding-top: theme.spacing.unit * 10;
+  padding-bottom: theme.spacing.unit * 10;
+  height: 100%;
+  margin: auto;
+`;
+
+const Card = ({ sentence }) => (
+  <StyledGrid>
     <StyledPaper>
-        <Typography variant='h5' component='h3'>
-        hello
-        </Typography>
+      <Typography variant='h5' component='h3'>
+        {sentence}
+      </Typography>
     </StyledPaper>
-    </StyledGrid>
+  </StyledGrid>
 );
 
 export default Card;
